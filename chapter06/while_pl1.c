a5887ec711d433f8d8afe988a86851231bcd93b3
jaehyeonzz <112991494+jaehyeonzz@users.noreply.github.com>
2022-11-07 10:34:27 +0900

    11.7

---
+++ chapter06/while_pl1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    //1~100±îÁö ÃÑÇÕ µ¡¼À(for)
+    int total = 0;
+    for(int i =1; i<=100; i++) {
+        total +=1;
+    }
+
+
+
+
+
+
+    // 1~100±îÁö ÃÑÇÕ µ¡¼À(while)
+    int num = 1;
+    while(num <=100) {
+        total += num;
+        num++;
+    }
+    printf("ÃÑÇÕ : %d\n", total);
+}
